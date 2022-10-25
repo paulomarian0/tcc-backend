b14e0549790a8178f4640a7f54c234784280bba7
Paulo Mariano <paulomariano2501@gmail.com>
2022-10-25 20:06:24 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3333;
+  app.enableCors();
   await app.listen(port);
   console.log(`Running in port ${port}`)
 }
